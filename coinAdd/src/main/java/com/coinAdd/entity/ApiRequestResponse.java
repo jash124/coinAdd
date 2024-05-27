0c73e7c2b72775874f77c8f91386e92411cc6805
Jash Bhadiyadra <jashbhadiyadra@gmail.com>
2024-05-27 17:11:36 +0530

    Updated size for length

--- coinAdd/src/main/java/com/coinAdd/entity/ApiRequestResponse.java
+++ coinAdd/src/main/java/com/coinAdd/entity/ApiRequestResponse.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +25,9 @@ public class ApiRequestResponse {
 
     private Long userId;
     private String requestUrl;
+    
+    @Lob
+    @Column(length = 65555)
     private String response;
 
     @Column(nullable = false)
